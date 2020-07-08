c63eddcdaff69c6559b7488f769f079ce00d008b
Bernard Shieh <bsauce@gmail.com>
2020-07-08 11:23:25 +0100

    Updated setup script

--- setup.py
+++ setup.py
@@ -1,26 +1,14 @@
-'''Setup script
+'''Setup script'''
 
-Author
--------
-
-Bernard Shieh (bernard.shieh@eng.ox.ac.uk)
-'''
-# from distutils.core import setup, Extension
 from setuptools import setup, find_packages
-# from setuptools.extension import Extension
-
-# import numpy as np
-# import os
 
 setup(
     name='pyfield',
-    version='0.1',
+    version='1.0',
     packages=find_packages(),
     package_data={
         'pyfield.core': ['*.m', '*.mat', '*.pdf', '*.mexw64', '*.mexa64']
     },
     #   entry_points={'console_scripts': ['pyfield = pyfield.cli:main']},
-    install_requires=['numpy', 'scipy', 'pytest'],
-    extras_require={
-        'interactive': ['matplotlib', 'jupyter', 'tqdm', 'ipywidgets']
-    })
+    install_requires=['numpy', 'scipy', 'matplotlib', 'pytest'],
+    extras_require={'interactive': ['jupyter', 'tqdm', 'ipywidgets']})
